2da6b2ff3541f9662f46805af19977e9fd81d401
Lev Nachmanson <levnach@hotmail.com>
2025-08-15 14:37:09 -0700

    define indexed root expression
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/nlsat/nlsat_explain.cpp
+++ src/nlsat/nlsat_explain.cpp
@@ -49,7 +49,7 @@ namespace nlsat {
         bool                    m_cell_sample;
 
         assignment const &      sample() const { return m_solver.sample(); }
-        assignment &      sample() { return m_solver.sample(); }
+        assignment &            sample() { return m_solver.sample(); }
 
         struct todo_set {
             polynomial::cache  &    m_cache;
